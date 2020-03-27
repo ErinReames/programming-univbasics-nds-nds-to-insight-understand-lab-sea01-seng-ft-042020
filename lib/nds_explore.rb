2bf78a08b035f7b4bb7ef964429d751a5669e24f
Erin Reames <italiagirl83@gmail.com>
2020-03-27 19:05:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,11 +9,5 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  i = 0 
-  movies = directors_database[0][:movies]
-  while i < movies.count 
-  {
-    puts movies[i][:title]
-    i +=1 
-  }
+  
 end
